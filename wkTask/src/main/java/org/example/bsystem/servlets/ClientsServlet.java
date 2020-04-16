927d0a58edbaeddbd05e4d5c2977c0a8f4a9a1df
Ivan Malinouski <IMalinouski@gomel.iba.by>
2020-04-16 23:05:10 +0300

    + clients servlet

---
+++ wkTask/src/main/java/org/example/bsystem/servlets/ClientsServlet.java
@@ -0,0 +1,45 @@
+package org.example.bsystem.servlets;
+
+import org.example.bsystem.logic.services.GettingClientsService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * process request for work with clients information
+ */
+@WebServlet("/clients")
+public class ClientsServlet extends HttpServlet {
+    private GettingClientsService service;
+
+    /**
+     * initializes servlet fields
+     * @param config servlet config
+     * @throws ServletException servlet exception
+     */
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init();
+        service = new GettingClientsService();
+    }
+
+    /**
+     * process get requests and writes a response string with a result
+     * @param req http request
+     * @param resp http response
+     * @throws ServletException servlet exception
+     * @throws IOException io exception
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String contentType = req.getParameter("content");
+        PrintWriter writer = resp.getWriter();
+        writer.write(service.getResult(contentType));
+    }
+}
